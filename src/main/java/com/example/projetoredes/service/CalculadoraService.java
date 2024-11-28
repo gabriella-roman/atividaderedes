de07a5f2b63e131d312c380cd00bef4b21d84bee
Gabriella <gabriellaperniconi@gmail.com>
2024-11-27 22:07:06 -0300

    mudando funcao

--- src/main/java/com/example/projetoredes/service/CalculadoraService.java
+++ src/main/java/com/example/projetoredes/service/CalculadoraService.java
@@ -1,14 +1,10 @@
 package com.example.projetoredes.service;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
-
 public class CalculadoraService {
   public double dividir(double a, double b) {
-    if(b == 0) {
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Não é possível dividir por zero");
+    if (b == 0) {
+      throw new IllegalArgumentException("Divisor não pode ser zero");
     }
-
     return a / b;
   }
 }
